48dd0ad84dc2dadcc2ff297690cc1b30fc1ee1c3
Mishra3 <Mohit_Mishra3@APAC.DELL.COM>
2018-06-15 16:25:53 +0530

    adding storage domain

--- assets/main.js
+++ assets/main.js
@@ -21,16 +21,10 @@ function startLiveAgentChat(buttonId) {
 function initSnapIn(deploymentId, buttonId, serviceTagVal, issueVal, browserLang, descriptionText) {
     if (!window.embedded_svc) { 
         var s = document.createElement('script');
-        debugger;
         s.setAttribute('src', 'https://dellservices--DEV3.cs20.my.salesforce.com/embeddedservice/5.0/esw.min.js');
         s.onload = function() { 
-            
-        }; 
-        $.getScript('https://dellservices--DEV3.cs20.my.salesforce.com/embeddedservice/5.0/esw.min.js', function()
-        {
             triggerSnapIn(null, deploymentId, buttonId, serviceTagVal, issueVal, browserLang, descriptionText); 
-        });
-        debugger;
+        };
         document.body.appendChild(s); 
         eleExist('.waitingCancelChat', chatCancelButtonEvent);
         eleExist('.waitingStateContent', waitingStateCallback);
